15c0b4b054b7ed5e35d7efc5d23f30fe9aae8b47
simohn <schauppinger@gmail.com>
2020-12-15 19:23:20 +0100

    Final update

--- UE5/FlexiblerRoboterarm/params.m
+++ UE5/FlexiblerRoboterarm/params.m
@@ -45,9 +45,10 @@ param_trajectory=[10,-pi/2,pi/2];
 A = [0 0 1 0; 0 0 0 1; -c / I__1 c / I__1 -d__1 / I__1 0; c / I__2 (m * g * l - c) / I__2 0 -d__2 / I__2;];
 B = [0 0 0.1e1 / I__1 0]';
 C = [0 1 0 0];
+D = 0;
 
 sys = ss(A,B,C,D);
-% rlocus(sys)
+rlocus(sys)
 
 % Damit alle Pole in linker offener s-Halbeben zu liegen kommen
 % 5< gain <10
--- UE5/Riementrieb/set_parameter.m
+++ UE5/Riementrieb/set_parameter.m
@@ -63,5 +63,55 @@ parReg.CCSFi = eye(2);
 parReg.DDSFi = zeros(2,1);
 parReg.ssSFi = ss(parReg.AASFi,parReg.BBSFi,parReg.CCSFi,parReg.DDSFi);
 
+%% Parameter der PI-Regler
 
-           
+I__l = parSys.Il;
+c__r1 = parSys.cr1;
+c__r3 = parSys.cr3;
+d__l = parSys.dl;
+r__l = parSys.rl;
+I__m = parSys.Im;
+d__m = parSys.dm;
+r__m = parSys.rm;
+R__m = parSys.Rm;
+L__m = parSys.Lm;
+p = parSys.p;
+Phi = parSys.Phi;
+
+% Arbeitspunkt
+M__l = 0;
+omega__md = 0;
+i__dd = 0;
+syms cg; %Variable zur Root Bestimmung
+Ae = [-0.1e1 / I__m * d__m 0.3e1 / 0.2e1 / I__m * p * Phi 0 0.2e1 / I__m * (3 * c__r3 * RootOf(2 * cg ^ 3 * c__r3 * r__l ^ 2 + 2 * cg * c__r1 * r__l ^ 2 + d__l * r__m * omega__md - M__l * r__l) ^ 2 + c__r1) * r__m 0; 0.2e1 / 0.3e1 / L__m * (0.3e1 / 0.2e1 * L__m * p * i__dd - p * Phi) -0.2e1 / 0.3e1 / L__m * R__m p * omega__md 0 0; 0.2e1 / 0.3e1 * (-d__l * r__m ^ 2 * omega__md - d__m * r__l ^ 2 * omega__md + M__l * r__l * r__m) / Phi / (r__l ^ 2) -p * omega__md -0.2e1 / 0.3e1 / L__m * R__m 0 0; -r__m 0 0 0 r__l; 0 0 0 -2 / I__l * (3 * c__r3 * RootOf(2 * cg ^ 3 * c__r3 * r__l ^ 2 + 2 * cg * c__r1 * r__l ^ 2 + d__l * r__m * omega__md - M__l * r__l) ^ 2 + c__r1) * r__l -1 / I__l * d__l;];
+Be = [0 0; 0.2e1 / 0.3e1 / L__m 0; 0 0.2e1 / 0.3e1 / L__m; 0 0; 0 0;];
+
+% Diagonale Übertragungsmatrix -> y1(u1), y2(u2)
+ss_euq = ss(Ae, Be(:,1), [1,0,0,0,0], 0);
+ss_eud = ss(Ae, Be(:,2), [0,0,1,0,0], 0);
+dss_euq = c2d(ss_euq, 1e-4);
+dss_eud = c2d(ss_eud, 1e-4);
+
+% Regler auslegen
+opts = pidtuneOptions('PhaseMargin',60);
+wc = 100; %rad/s
+[PI_euq, infoq] = pidtune(dss_euq, 'PI', wc, opts);
+[PI_eud, infod] = pidtune(dss_eud, 'PI', wc, opts);
+parPI_euq = ss(PI_euq);
+parPI_eud = ss(PI_eud);
+
+parPI.Aq = parPI_euq.A;
+parPI.Bq = parPI_euq.B;
+parPI.Cq = parPI_euq.C;
+parPI.Dq = parPI_euq.D;
+parPI.Ad = parPI_eud.A;
+parPI.Bd = parPI_eud.B;
+parPI.Cd = parPI_eud.C;
+parPI.Dd = parPI_eud.D;
+
+
+%% RootsOf aus Maple lösen
+function epsilon__d = RootOf(equ)
+roots = solve(equ == 0, 'Real', true);
+epsilon__d = double(roots(1));
+end
